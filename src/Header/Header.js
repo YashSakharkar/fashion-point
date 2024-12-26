cfdd48f211c4b2c3628388095a50e2705dfeaecc
unknown <YashSakharkar>
2024-12-26 20:20:24 +0530

    commit 4

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -6,15 +6,20 @@ const Header = () => {
   return (
     <div className='header'>
        <div className='container'>
+       <nav className='header-menu'>
+              <ul className='menu-bar'>
+                 <li>Home</li>
+                 <li>About-Us</li>
+                 <li>Contact</li>
+              </ul>
+          </nav>
           <div className='logo-img'>
              <img src="./logo-image-removebg-preview.png" alt='Image'/>
           </div>
           <nav className='header-menu'>
               <ul className='menu-bar'>
-                 <li>Home</li>
-                 <li>About-Us</li>
-                 <li>Contact</li>
-                 <li>Services</li>
+              <li className='language-logo'><img src='./Flag_of_India.svg.webp'/><span> EN</span></li>
+              <li>Returns & Orders</li>
                  <li><Face6Icon sx={{color: 'aqua', fontSize: 30}}/></li>
                  <li><ShoppingCartIcon sx={{color: 'aqua',fontSize: 30}}/></li>
               </ul>
