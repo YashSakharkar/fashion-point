2f5b830fa50dae6e61f0773182ae66bff428f6de
Sahil-Parbat <sahilparbat24@gmail.com>
2024-12-30 18:39:56 +0530

    Card and feature product

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -1,102 +1,37 @@
 import React from "react";
 import "./Card.css";
+import CardImages from "./CardImages";
 
 const Card = () => {
   return (
-    <div className="card">
-      <div className="card-container">
+    
+    <div className="card" >
+    <div className="card-container">
+      {
+        
+        CardImages.map((image) => {
+        return (
+          
+            
+              <div className="box">
+                <img src={image.cardImage} alt={image.alt} />
 
+                <div className="over-img">
+                  <h2>{image.cardHeading}</h2>
+                  <p>{image.cardDescription}</p>
 
-        <div className="box">
-          <img src={require("./women-fashion.jpg")} alt="women-fashion" />
-
-          <div className="over-img">
-            <h2>20% off on Tank tops</h2>
-            <p>
-              Upgrade your summer style with 20% off on tank tops-don't miss
-              out!
-            </p>
-
-            <div className="shopnow-btn">
-              <b>SHOP NOW</b>
-            </div>
-          </div>
-        </div>
-        <div className="box">
-          <img src={require("./men-fashion.jpg")} alt="men-fashion" />
-          <div className="over-img">
-            <h2>Latest Eyewear</h2>
-            <p>
-              Upgrade your summer style with 20% off on tank tops-don't miss
-              out!
-            </p>
-
-            <div className="shopnow-btn">
-              <b>SHOP NOW</b>
-            </div>
-          </div>
-        </div>
-        <div className="box">
-          <img src={require("./footwear.jpg")} alt="Footwear" />
-          <div className="over-img">
-            <h2>Let's Lorem Suit Up!</h2>
-            <p>
-              Upgrade your summer style with 20% off on tank tops-don't miss
-              out!.
-            </p>
-
-            <div className="shopnow-btn">
-              <b>SHOP NOW</b>
-            </div>
-          </div>
-        </div>
-
-        <div className="box">
-          <img src={require("./women-fashion.jpg")} alt="women-fashion" />
-
-          <div className="over-img">
-            <h2>20% off on Tank tops</h2>
-            <p>
-              Upgrade your summer style with 20% off on tank tops-don't miss
-              out!
-            </p>
-
-            <div className="shopnow-btn">
-              <b>SHOP NOW</b>
-            </div>
-          </div>
-        </div>
-        <div className="box">
-          <img src={require("./men-fashion.jpg")} alt="men-fashion" />
-          <div className="over-img">
-            <h2>Latest Eyewear</h2>
-            <p>
-              Upgrade your summer style with 20% off on tank tops-don't miss
-              out!
-            </p>
-
-            <div className="shopnow-btn">
-              <b>SHOP NOW</b>
-            </div>
-          </div>
-        </div>
-        <div className="box">
-          <img src={require("./footwear.jpg")} alt="Footwear" />
-          <div className="over-img">
-            <h2>Let's Lorem Suit Up!</h2>
-            <p>
-              Upgrade your summer style with 20% off on tank tops-don't miss
-              out!.
-            </p>
-
-            <div className="shopnow-btn">
-              <b>SHOP NOW</b>
-            </div>
-          </div>
-        </div>
+                  <div className="shopnow-btn">
+                    <b>SHOP NOW</b>
+                  </div>
+                </div>
+              </div>
+            
+        );
+      })}
 
       </div>
-    </div>
+          </div>
+    
   );
 };
 
